64f503dc021a409590e78926c55058e2c21546f4
Toni Ros <33701571+ToniRos@users.noreply.github.com>
2021-03-29 12:34:34 +0200

    Activate Debug

--- DahuaVTO.py
+++ DahuaVTO.py
@@ -15,7 +15,7 @@ from requests.auth import HTTPDigestAuth
 
 from Messages import MessageData
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 log_level = logging.DEBUG if DEBUG else logging.INFO
 
